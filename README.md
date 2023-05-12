5b84bc81ff81d4e5ec6a36bc46f2d28fde9e6b0a
Mateus Cavalheiro <mateuscavalheiro.prog@gmail.com>
2023-05-12 00:31:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# teste-api-key-accuweather
---
+++ js/weather.js
@@ -0,0 +1,4 @@
+const apiKey = 'GvXNN2iSASmPVSPvXLGGrPCV5HTLwLB6'
+const baseUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=curitiba`
+
+fetch(baseUrl)
